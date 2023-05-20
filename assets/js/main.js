e9617b6c213062e45d51aca0df0d4cf87d6b5015
Tomas Paul Cservenak <tomas.cservenak@gmail.com>
2023-05-20 08:20:57 -0600

    experiment

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,29 @@ AOS.init({
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
 });
+
+// Get Modal
+var modal = document.getElementById('myModal');
+    
+// Get pseudoelement to open Modal
+var btn = document.getElementById("sized");
+
+// Get the <span> element to close Modal
+var span = document.getElementsByClassName("close")[0];
+
+// When user clicks button, open Modal
+btn.onclick = function() {
+   modal.style.display = "block";
+   };
+
+// When user clicks Close (x), close Modal
+span.onclick = function() {
+   modal.style.display = "none";
+   };
+
+// When user clicks anywhere outside of the Modal, close Modal
+ window.onclick = function(event) {
+    if (event.target == modal) {
+       modal.style.display = "none";
+       }
+    };
